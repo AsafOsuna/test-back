d5fcc2cf278308581fe00124afc961395f56852d
Asaf <aosuna@cybermax.com.mx>
2023-07-24 08:37:26 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require ("http")
+
+function requestController(){
+    //Logica de la función
+    console.log("Hola mundo")
+}
+
+// Configurar nuestro servidor
+const server = http.createServer(requestController)
+
+server.listen(4000)
