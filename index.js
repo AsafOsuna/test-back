470f8b5a20a935771e9a74c4c141744fedf48e5b
Asaf <aosuna@cybermax.com.mx>
2023-07-27 10:11:45 -0600

    Agregamos Variables de Entorno

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const dotenv = require ("dotenv")
+dotenv.config()
 const http = require ("http")
 
 function requestController(){
     //Logica de la función
-    console.log("Hola mundo!")
+    console.log("Hola mundo!!")
 }
 
 // Configurar nuestro servidor
 const server = http.createServer(requestController)
 
-server.listen(4000)
+const PORT = process.env.PORT
+server.listen(PORT, function (){
+    console.log("Aplication corriendo en puerto: " + PORT)
+})
